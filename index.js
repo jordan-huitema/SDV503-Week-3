29f0e3a5f29e12c15f5dc46b332f28107339d8e2
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-03-09 13:28:35 +1300

    adding more additoin operation styles

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ console.log(x / y);  //x divided by y
 console.log(x % y);  //remainder of x and y
 console.log(x == y); //does x = y?
 
+/* Addition Operation in three styles */
+
+//let x = 10;
+//let y = 5; 
+console.log(x + y);
+
+//let x = 10;
+//let y = 5;
+let result = x + y;
+console.log(result);
+
+let calc = 10 + 5;
+console.log(calc);
