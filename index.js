c9f32264c249d90c8b04cb7feb96c864980730c4
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-03-09 13:23:14 +1300

    lesson part 1

---
+++ index.js
@@ -0,0 +1,12 @@
+//setting variables
+let x = 10;
+let y = 5;
+
+//printing  to console
+console.log(x + y);  //sum of x and y
+console.log(x - y);  //subtraction of x and y
+console.log(x * y);  //multiplication of x and y
+console.log(x / y);  //x divided by y
+console.log(x % y);  //remainder of x and y
+console.log(x == y); //does x = y?
+
