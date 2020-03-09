7f46ac4cf414df151f60bf2fd5391625e1d02a6a
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-03-09 13:50:35 +1300

    adding pre increment and post increment, pre adds 1 before before the code is executed and post adds 1 after the line is executed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ Operator             Description
    %                  Modulus
   **                 Exponentiation
 */
-
+/*
 //setting variables
 let x = 10;
 let y = 5;
@@ -19,9 +19,9 @@ console.log(x - y);  //subtraction of x and y
 console.log(x * y);  //multiplication of x and y
 console.log(x / y);  //x divided by y
 console.log(x % y);  //remainder of x and y
-console.log(x == y); //does x = y?
+console.log(x ** y); //x to the power of y
 
-/* Addition Operation in three styles */
+//Addition Operation in three styles
 
 //let x = 10;
 //let y = 5; 
@@ -34,5 +34,25 @@ console.log(result);
 
 let calc = 10 + 5;
 console.log(calc);
-
+*/
 /*----------Part 1.1----------*/
+// increment Operator 
+let x = 10;
+//pre increment
+console.log(x);
+console.log(++x);   //increasing x by 1
+console.log(x);
+console.log(++x);   //increasing x by 1
+console.log(x);
+console.log(++x);   //increasing x by 1
+console.log(x);
+//post increment
+let y = 10;
+
+console.log(y);
+console.log(y++);   //increasing x by 1 
+console.log(y);
+console.log(y++);   //increasing x by 1
+console.log(y);
+console.log(y++);   //increasing x by 1
+console.log(y);
